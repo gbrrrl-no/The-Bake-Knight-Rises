aa9d0f7612bf112cf137ae2551c9360b005b4749
Victor Hugo <vhlk@cin.ufpe.br>
2021-08-25 10:26:01 -0300

    spawn enemy pig

--- The Baker Knight Rises/Assets/Scripts/GameManager.cs
+++ The Baker Knight Rises/Assets/Scripts/GameManager.cs
@@ -8,6 +8,33 @@ public class GameManager : MonoBehaviour
 
     public GameObject CompleteLevelUI;
 
+    public GameObject enemyPig;
+    public const int numMaxOfEnemies = 5;
+    private int numOfEnemies = 0;
+    private float lastEnemyCreated = 0;
+
+    private void Start()
+    {
+        CreateEnemyPig();
+    }
+
+    private void Update()
+    {
+        // criar inimigo a cada 5 segundos
+        if (Time.timeSinceLevelLoad > lastEnemyCreated + 5 && numOfEnemies <= numMaxOfEnemies)
+        {
+            CreateEnemyPig();
+        }
+    }
+
+    private void CreateEnemyPig()
+    {
+        GameObject enemyPigGO = Instantiate(enemyPig);
+        enemyPigGO.SetActive(true);
+        lastEnemyCreated = Time.timeSinceLevelLoad;
+        numOfEnemies++;
+    }
+
     public void CompleteLevel() {
         CompleteLevelUI.SetActive(true);
     }
