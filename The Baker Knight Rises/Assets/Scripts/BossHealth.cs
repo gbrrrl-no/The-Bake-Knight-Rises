8203226b3eae72316497813a27dac4e09659e472
Gabriela Heimer <gvh@cin.ufpe.br>
2021-08-25 18:27:10 -0700

    add boss health bar

--- The Baker Knight Rises/Assets/Scripts/BossHealth.cs
+++ The Baker Knight Rises/Assets/Scripts/BossHealth.cs
@@ -6,12 +6,19 @@ public class BossHealth : MonoBehaviour
 {
 
 	public int health = 500;
+    public HealthBar healthBar;
+
+    void Start()
+    {
+        healthBar.SetMaxHealth(health);
+    }
 
 	public void TakeDamage(int damage)
 	{
 
 		health -= damage;
-        
+        healthBar.SetHealth(health);
+
 		if (health <= 0)
 		{
 			Die();
